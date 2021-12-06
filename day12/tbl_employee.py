2c28a185cc821ef8f637cbdd9a7b21d131881ef5
sharpboy77 <sharpboy77@naver.com>
2021-12-06 12:53:00 +0900

    day13

--- day12/tbl_employee.py
+++ day12/tbl_employee.py
@@ -1,23 +1,82 @@
 import sqlite3
-#데이터 조회
+
+#중복되는 부분 정리
+def getconn():
+    con = sqlite3.connect("c:/pydb/mydb.db") #다른 곳에 있는 것을 네트워크 연결을 통해 자료를 가져옴, mydb에 db가 없어도 여기서 만들어짐)
+    return con
+
+#전체 자료 검색
 def select_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db") #db 연결 객체 생성
+    conn = getconn() #db 연결 객체 생성
     cur = conn.cursor() #db 작업 개체
-    sql = "SELECT * FROM employee" #db 검색
+    sql = "SELECT * FROM employee ORDER BY emp_id" #db 검색
     cur.execute(sql) #검색 실행
     rs = cur.fetchall() #db 저장된 모든 데이터 가져오기
     #print(rs) #전체 출력
     for i in rs: #각각 출력
         print(i)
     conn.close() #db 연결 종료
-#데이터 추가
+
+#자료 추가
 def insert_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db")
+    conn = getconn()
     cur = conn.cursor()
-    sql = "INSERT INTO employee VALUES ('e1002', '손흥민', 30, 50000)" #db 추가
-    cur.execute(sql)
+    #데이터 입력방법 1. 직접 입력
+    #sql = "INSERT INTO employee VALUES ('e1006', '박찬호', 40, 100000)" #db 추가
+    #cur.execute(sql)
+    #conn.commit()  # 변경 사항이 생겼을 때 필수
+    #conn.close()
+
+    #데이터 입력방법 2. 동적 입력('?' 사용)
+    sql = "INSERT INTO employee VALUES (?, ?, ?, ?)"  # db 추가
+    cur.execute(sql, ('e1008', '박지성', 19, 70000))
     conn.commit() #변경 사항이 생겼을 때 필수
     conn.close()
 
-#insert_emp() #처음 추가 할 때 열어두고, 다음 작업 시 닫아둔다
-select_emp()
+    #데이터 입력방법 3. 리스트로 입력(추후)
+
+#특정 자료 검색
+#방법 1.
+#def select_one():
+    #conn = getconn()
+    #cur = conn.cursor()
+    #sql = "SELECT emp_id, name FROM employee WHERE salary = 70000"
+    #cur.execute(sql)
+    #rs = cur.fetchone() #1명의 자료 반환(여러 명의 자료값이 나올 때는 반복문을 써서)
+    #print(rs)
+    #conn.close()
+
+#방법 2.
+def select_one():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT emp_id, name FROM employee WHERE emp_id=?"
+    cur.execute(sql, ('e1008', )) #튜플 구조는 콤마를 찍어야 함
+    rs = cur.fetchone() #1명의 자료 반환(여러 명의 자료값이 나올 때는 반복문을 써서)
+    print(rs)
+    conn.close()
+
+#자료 수정
+def update_emp():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "UPDATE employee SET age=? WHERE emp_id=?"
+    cur.execute(sql, (35, 'e1004'))
+    conn.commit()
+    conn.close()
+
+#자료 삭제
+def delete_emp():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM employee WHERE name=?"
+    cur.execute(sql, ('안산',))
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__": #이 모듈을 다른 파일에서 활용할 때 출력 파일에서 이 내용이 나오지 않게끔 하기 위함
+    #insert_emp() #처음 추가 할 때 열어두고, 다음 작업 시 닫아둔다(계속 열어두고 실행하면 계속 추가되기 때문)
+    #select_one() #방법 2의 호출
+    update_emp()
+    #delete_emp()
+    select_emp()
---
+++ day13/tbl_book.py
@@ -0,0 +1,84 @@
+#book테이블 만들기
+import sqlite3
+
+def getconn():
+    con = sqlite3.connect('./output/sample.db')
+    return con
+
+def create_table():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = """
+    CREATE TABLE book(
+        book_no INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        publisher TEXT NOT NULL,
+        page INTEGER
+    )
+    """
+    #AUTOINCREMENT - 자동 순번(sequence) db브라우저에서 하나의 테이블로 인식
+    cur.execute(sql)
+    conn.commit()
+    print("book테이블 생성")
+    conn.close()
+
+#def insert_book():
+    #conn = getconn()
+    #cur = conn.cursor()
+    #sql = "INSERT INTO book (title, publisher, page) VALUES ('웹 표준의 정석', '고경희', '600')" #book_no는 자동 순번 입력되게끔 했으므로 제외하고 이 제외되면서 employee 뒤에 칼럼명 명시해서 맞춰준다
+    #cur.execute(sql)
+    #conn.commit()
+    #conn.close()
+
+#동적 입력
+def insert_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "INSERT INTO book (title, publisher, page) VALUES (?, ?, ?)" #book_no는 자동 순번 입력되게끔 했으므로 제외하고 이 제외되면서 employee 뒤에 칼럼명 명시해서 맞춰준다
+    cur.execute(sql, ('점프 투 파이썬', '박응용', 300))
+    conn.commit()
+    conn.close()
+
+def select_book(): #db브라우저에서 확인 안 하고자
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM book"
+    cur.execute(sql)
+    rs = cur.fetchall()
+    #print(rs)
+    for i in rs:
+        print(i)
+    conn.close() #commit이 필요없음
+
+def select_one(): ##현재 오류##
+    conn = getconn()
+    cur = conn.cursor
+    sql = "SELECT * FROM book WHERE book_no=?" #책 번호로 검색
+    cur.execute(sql, (1, ))
+    rs = cur.fetchone()
+    print(rs)
+    conn.close()
+
+def update_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "UPDATE book SET page=? WHERE book_no=?" #page=300->350 변경
+    cur.execute(sql, (350, 3)) #page, book_no순으로 작성
+    conn.commit()
+    conn.close()
+
+def delete_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM book WHERE book_no=?"  #book_no=1인 책 삭제
+    cur.execute(sql, (1,))
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    #create_table()
+    #insert_book()
+    #select_one()
+    update_book()
+    #delete_book()
+    select_book()
